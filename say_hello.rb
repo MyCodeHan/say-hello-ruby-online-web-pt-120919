e2cb86cc72810775bfe474020971213ebe6c0b20
Hannah Harrington <hannahharrington@outlook.com>
2019-12-10 22:49:51 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,6 +3,6 @@ def say_hello(name)
   puts "Hello #{name}!"
 end
 
-def say_hello(name = "Ruby programmer")
-  puts "Hello, #{name}"
+def say_hello(language = "Ruby programmer")
+  puts "Hello, #{language}"
 end
